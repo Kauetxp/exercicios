18f7db7604e1e33b16d632853009219295386e8b
Kauetxp <123607569+Kauetxp@users.noreply.github.com>
2023-02-11 15:37:22 +0000

    exercicios

---
+++ Ex5.py
@@ -0,0 +1,17 @@
+menu = str(input("0 - FIM\n1 - Inclui\n2 - Altera\n3 - Exclui\n4 - Consulta\n"))
+
+
+
+if menu == "0":
+    print("FIM")
+elif menu == "1":
+    print("INCLUI")
+elif menu == "2":
+    print("ALTERA")
+elif menu == "3":
+    print("EXCLUI")
+elif menu == "4":
+    print("CONSULTA")
+else:
+    print("Digito invalido")
+
---
+++ Ex6.py
@@ -0,0 +1,3 @@
+x = input("Digite um caracter: ")
+
+print("              ",x,"\n            ",x,x,x,"\n          ",x,x,x,x,x,"\n        ",x,x,x,x,x,x,x,"\n      ",x,x,x,x,x,x,x,x,x,"\n    ",x,x,x,x,x,x,x,x,x,x,x,"\n  ",x,x,x,x,x,x,x,x,x,x,x,x,x,"\n",x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,"\n","            ",x,x,"\n","            ",x,x,"\n""           ",x,x,x,x,"\n")
